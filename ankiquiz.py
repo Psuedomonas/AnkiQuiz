7401a0b8057a64e1b11d48bfdcf71cd2de365827
Pseudomonas <zehm0007@d.umn.edu>
2020-07-17 13:39:37 -0500

    Create ankiquiz.py

---
+++ ankiquiz.py
@@ -0,0 +1,43 @@
+'''
+Author: Nicholas A Zehm
+Filename:
+7-11-20
+
+TODO: Switch to frames?
+'''
+
+from tkinter import * #GUI
+import random #random numbers
+
+class Window(Frame):
+	def __init__(self, master=None):
+		Frame.__init__(self, master=None)
+		self.master = master
+		self.init_window()
+	
+	def init_window(self):
+		self.master.title("Word to the Perd")
+		self.pack(fill=BOTH, expand=1)
+		
+		#menu
+		menu = Menu(self.master)
+		self.master.config(menu=menu)
+		mainMenu = Menu(menu)
+		
+		#Menu options
+		mainMenu.add_command(label="Quess the word", command=)
+		mainMenu.add_command(label="Define the word", command=)
+		mainMenu.add_command(label="Match dem up", command=)
+		mainMenu.add_command(label="Exit", command=lambda : self.client_exit)
+		
+		menu.add_cascade(label=Main menu", menu=mainMenu)
+		
+		text = Text(root)
+		
+	def client_exit(self):
+		exit()
+
+root = Tk()
+root.geometry("800x600)
+app = Window(root)
+root.mainloop()
